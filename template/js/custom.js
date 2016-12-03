1d5b749129bcb449bc308a46fb7c3b073b611aa0
alkazua <alan.meile@stud.hslu.ch>
2016-12-03 13:15:53 +0100

    Template for our GUI, in HTML.

---
+++ template/js/custom.js
@@ -0,0 +1,22 @@
+$(function() {    
+    /* Serie Cards */
+    $('.chips-placeholder').material_chip({
+        placeholder:            '+Tag',
+        secondaryPlaceholder:   'Restrict series to'
+    })
+    $('#tag-search').append("<i class='material-icons'>search</i>");
+    
+    /**
+     ** ################## User Action ################## */
+    $('.chips').on('chip.add', function(e, chip){
+        console.log("Chip added: ", chip["tag"]);
+    });
+
+    $('.chips').on('chip.delete', function(e, chip){
+        console.log("Chip removed: ", chip["tag"]);
+    });
+
+    $('.chips').on('chip.select', function(e, chip){
+        console.log("Chip selected: ", chip["tag"]);
+    });
+});
